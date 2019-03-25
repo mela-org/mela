5c071f38e9c389ce7b93d3fbf1a7483b58f99ddc
Stupreme <justuskliem@gmail.com>
2019-03-25 21:35:48 +0100

    Add Javadoc for Cassandra interface

--- src/main/java/com/github/stupremee/mela/cassandra/Cassandra.java
+++ src/main/java/com/github/stupremee/mela/cassandra/Cassandra.java
@@ -1,6 +1,7 @@
 package com.github.stupremee.mela.cassandra;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
 import com.datastax.driver.mapping.MappingManager;
@@ -16,17 +17,57 @@ import reactor.core.publisher.Mono;
 @SuppressWarnings("unused")
 public interface Cassandra {
 
-  ResultSet execute(String statement);
+  /**
+   * Executes a CQL Statement synchronously and returns the result set.
+   *
+   * @param statement The CQL Statement as {@link PreparedStatement}
+   * @return The Result set
+   */
+  ResultSet execute(PreparedStatement statement);
 
-  Mono<ResultSet> executeAsync(String statement);
+  /**
+   * Executes a CQL Statement asynchronously and returns the result set in a {@link Mono}.
+   *
+   * @param statement The CQL Statement as {@link PreparedStatement}
+   * @return The Result set in a {@link Mono}
+   */
+  Mono<ResultSet> executeAsync(PreparedStatement statement);
 
+  /**
+   * Returns the cluster from the cassandra database.
+   *
+   * @return The {@link Cluster}
+   */
   Cluster cluster();
 
+  /**
+   * Returns the session.
+   *
+   * @return The {@link Session}
+   */
   Session session();
 
+  /**
+   * Returns the mapping manager to create accessors, {@link ObjectMapper ObjectMappers} etc.
+   *
+   * @return The {@link MappingManager}
+   */
   MappingManager mapper();
 
+  /**
+   * Creates a new {@link ObjectMapper} from the clazz that is given in the paramter.
+   *
+   * @param clazz The {@link Class} that the {@link ObjectMapper} should map
+   * @return The {@link ObjectMapper}
+   */
   ObjectMapper mapper(Class<?> clazz);
 
+  /**
+   * Creates a accessor via the {@link MappingManager}
+   *
+   * @param clazz The accessor class
+   * @param <T> The type of the class
+   * @return The accessor
+   */
   <T> T accessor(Class<T> clazz);
 }
