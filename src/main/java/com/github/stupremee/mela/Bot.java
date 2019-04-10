9ba45065b10df3e3bdf19699952b004d9fe79a56
Stupreme <justuskliem@gmail.com>
2019-04-10 16:13:25 +0200

    Whoops. Forgot to remove debug stuff

--- src/main/java/com/github/stupremee/mela/Bot.java
+++ src/main/java/com/github/stupremee/mela/Bot.java
@@ -1,11 +1,5 @@
 package com.github.stupremee.mela;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.model.Filters;
-import org.bson.BsonDocument;
-import org.bson.conversions.Bson;
-import org.bson.json.JsonWriterSettings;
-
 /**
  * https://github.com/Stupremee
  *
@@ -13,15 +7,4 @@ import org.bson.json.JsonWriterSettings;
  * @since 23.03.2019
  */
 public class Bot {
-
-  public static void main(String[] args) {
-    print(Filters.or(Filters.eq("key"), Filters.exists("content")));
-  }
-
-  private static void print(Bson filter) {
-    System.out
-        .println(filter.toBsonDocument(BsonDocument.class, MongoClient.getDefaultCodecRegistry())
-            .toJson(JsonWriterSettings.builder().indent(true).build()));
-  }
-
 }
