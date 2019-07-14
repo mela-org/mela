eff4a748ab1e707184ac8b9351836c03c6708807
Justus K <justus.k@protonmail.com>
2019-07-14 22:17:45 +0200

    Annotate Listener with @FunctionalInterface

--- mela-event/src/main/java/com/github/stupremee/mela/event/Listener.java
+++ mela-event/src/main/java/com/github/stupremee/mela/event/Listener.java
@@ -1,13 +1,16 @@
 package com.github.stupremee.mela.event;
 
 /**
+ * This listener listens to all events.
+ *
  * @author Stu (https://github.com/Stupremee)
  * @since 14.07.19
  */
+@FunctionalInterface
 public interface Listener {
 
   /**
-   * This method gets called when a event was posted
+   * This method gets called when a event was posted.
    */
   void onEvent(Object event);
 
