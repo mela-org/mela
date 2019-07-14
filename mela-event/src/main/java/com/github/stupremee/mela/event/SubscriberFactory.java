bb2467109adbd8e9898a1618d68fa5bb2c3de528
Justus K <justus.k@protonmail.com>
2019-07-14 22:47:45 +0200

    Add SubscriberFactory interface

---
+++ mela-event/src/main/java/com/github/stupremee/mela/event/SubscriberFactory.java
@@ -0,0 +1,40 @@
+package com.github.stupremee.mela.event;
+
+import java.util.function.Consumer;
+
+/**
+ * This interface will create a {@link Subscriber} on different ways.
+ *
+ * @author Stu (https://github.com/Stupremee)
+ * @since 14.07.19
+ */
+public interface SubscriberFactory {
+
+  /**
+   * Creates a {@link Subscriber} by searching all methods in the given listener which are annotated
+   * with {@link com.github.stupremee.mela.event.annotations.Subscribe}.
+   */
+  Subscriber fromListener(Object listener);
+
+  /**
+   * Creates a {@link Subscriber} from the given {@code listener}.
+   */
+  Subscriber fromListener(Listener listener);
+
+  /**
+   * Creates a {@link Subscriber} from the given {@code listener}.
+   */
+  <T> Subscriber fromListener(GenericListener<T> listener);
+
+  /**
+   * Creates a {@link Subscriber} from the given {@link Consumer callback} which will be called on
+   * every event.
+   */
+  Subscriber fromCallback(Consumer<Object> callback);
+
+  /**
+   * Creates a {@link Subscriber} from the given {@link Consumer callback} which will be called on
+   * every event of the given {@code eventType}.
+   */
+  <T> Subscriber fromCallback(Class<T> eventType, Consumer<T> callback);
+}
